f690820ed651d4286268f4eedc515c1792804496
Muhammad Amir <maamirali555@gmail.com>
2024-09-03 15:33:36 +0500

    my-new-php-file.php

--- git.php
+++ git.php
@@ -1,3 +1,4 @@
 <?php
 echo "string";
+// echo 'helo kaka';
 ?>
---
+++ my-new-php-file.php
@@ -0,0 +1,20 @@
+<?php
+echo "string";
+// echo 'helo kaka';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		hel
+	</title>
+</head>
+<body>
+	<div>
+		<h1> MY Git and git hub file h</h1>
+	</div>
+
+</body>
+</html>
